92558f9531f030a883a0d488c617caf20aeba382
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2021-07-25 22:15:59 -0400

    Fixed date map implementation. Still needs work.

--- displayNewDateMap.js
+++ displayNewDateMap.js
@@ -1,13 +1,13 @@
 var path = 'https://theseanfraser.github.io/HighParkBirdLocations/maps/checklists/';
-var selectSpeciesButton = document.getElementById("selectDateButton");
+var selectDateButton = document.getElementById("selectDateButton");
 
 
-selectSpeciesButton.onclick = function() {
+selectDateButton.onclick = function() {
 	console.log("Button clicked");
 	var dateMapiFrame = document.getElementById("date_map");
 	var date = document.getElementById("dateSelector").value;
 	//var mapTitle = document.getElementById("mapTitle");
-    var source = path + date + ' Map.html';
+    var source = path + date + '.html';
 	console.log(source);
 	//mapTitle.innerHTML("Map of " + speciesName +  " seen since July 2021:");
     dateMapiFrame.setAttribute('src', source);
