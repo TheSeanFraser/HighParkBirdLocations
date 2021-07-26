26293c5a000a4e0659005d1fb70845a39f7f6041
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2021-07-26 11:33:13 -0400

    Added fragment functionality to allow direct links to each species. Also updated about.html

--- displayNewSpeciesMap.js
+++ displayNewSpeciesMap.js
@@ -6,11 +6,33 @@ selectSpeciesButton.onclick = function() {
 	console.log("Button clicked");
 	var speciesMapiFrame = document.getElementById("species_map");
 	var speciesName = document.getElementById("speciesSelector").value;
-	//var mapTitle = document.getElementById("mapTitle");
+	
     var source = path + speciesName + '.html';
 	console.log(source);
-	//mapTitle.innerHTML("Map of " + speciesName +  " seen since July 2021:");
+	var mapTitle = document.getElementById("mapTitle");
+	mapTitle.innerHTML = "Map of " + speciesName +  " observations since July 2021:";
+	
     speciesMapiFrame.setAttribute('src', source);
 };
 
 
+// Checks and deals with hashtag fragment in link
+function fragmentSpeciesMap(){
+	if(window.location.hash) {
+		console.log("HASH FOUND");
+		var hashSpeciesName = decodeURI(window.location.hash.split("#")[1]);
+		var speciesMapiFrame = document.getElementById("species_map");
+		var mapTitle = document.getElementById("mapTitle");
+		var source = path + hashSpeciesName + '.html';
+		
+		console.log(source);
+		mapTitle.innerHTML = "Map of " + hashSpeciesName +  " observations since July 2021:";
+		document.getElementById("speciesSelector").value = hashSpeciesName;
+		speciesMapiFrame.setAttribute('src', source);
+		
+	} else {
+		console.log("NO HASH");
+	}
+}
+
+fragmentSpeciesMap();
