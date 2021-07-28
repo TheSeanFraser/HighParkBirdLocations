79a2efbe41463c85fc40ac8d76f053318d75ccb9
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2021-07-28 15:55:46 -0400

    Updated fragment support for species charts.

--- displayNewSpeciesMap.js
+++ displayNewSpeciesMap.js
@@ -32,13 +32,20 @@ function fragmentSpeciesMap(){
 		console.log("HASH FOUND");
 		var hashSpeciesName = decodeURI(window.location.hash.split("#")[1]);
 		var speciesMapiFrame = document.getElementById("species_map");
+		var barChartiFrame = document.getElementById("bar_chart");
+		var pieChartiFrame = document.getElementById("pie_chart");
+
 		var mapTitle = document.getElementById("mapTitle");
 		var source = mapPath + hashSpeciesName + '.html';
+		var barChartSource = chartPath + hashSpeciesName + ' Bar Graph.html'
+		var pieChartSource = chartPath + hashSpeciesName + ' Pie Graph.html'
 		
 		console.log(source);
 		mapTitle.innerHTML = "Map of " + hashSpeciesName +  " observations since July 2021:";
 		document.getElementById("speciesSelector").value = hashSpeciesName;
 		speciesMapiFrame.setAttribute('src', source);
+		barChartiFrame.setAttribute('src', barChartSource);
+		pieChartiFrame.setAttribute('src', pieChartSource);
 		
 	} else {
 		console.log("NO HASH");
