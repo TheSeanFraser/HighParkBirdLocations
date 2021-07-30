2a3ce3a9a2320228fab7891870649c58af5fdf35
Sean Fraser <7241905+TheSeanFraser@users.noreply.github.com>
2021-07-30 13:22:32 -0400

    Delete gtag.js

--- gtag.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-ZLPYC17WV2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-ZLPYC17WV2');
-</script>
